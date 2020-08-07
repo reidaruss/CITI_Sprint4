eca5407d2d3ca12d4da402be7ec5bc318d49348e
alyssa cervantes <alyssac1003@gmail.com>
2020-08-06 17:08:07 -0700

    display of homepage and info page

--- routes.py
+++ routes.py
@@ -2,11 +2,18 @@
 from flask import Flask
 from flask import send_file
 from flask import request, session
+# flask_cors.CORS(routes, expose_headers='Authorization')
+import requests
+from tabulate import tabulate
 
+# import the Flask class from the flask module
+from flask import Flask, Response, send_from_directory
+import logging
+import re 
 # from flask_cors import CORS, cross_origin
 routes = Flask(__name__)
 
-from flask import request, jsonify, Response, render_template
+from flask import request, jsonify, render_template
 
 
 import numpy as np
@@ -15,25 +22,46 @@ import os
 # import logging
 # from flask_cors import CORS, cross_origin
 
-
+#35.226.134.37:5000/upload
 @routes.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
-
-@routes.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-
-        return "This is a POST"
-
-    else:
-        return "This is a GET bro"
-
+@routes.route('/<string:result>', methods=['GET'])
+def get_result(result):
+    # news_list = get_news(result)
+    return render_template('search_result.html', name = result)
 
 if __name__ == "__main__":
     # routes.run(host='0.0.0.0')
+ 
     routes.secret_key = os.urandom(24)
     routes.run(host="0.0.0.0", use_reloader=False)
 
-# flask_cors.CORS(routes, expose_headers='Authorization')
+
+
+# ##############################################################
+# CREATING THE SEARCH FUNCTION USING API
+def get_news(ticker):
+
+
+    url = ('http://newsapi.org/v2/everything?'
+        'q=' + str(ticker) + '&'
+        'from=2020-08-05&'
+        'sortBy=popularity&'
+        'apiKey=5092bd320d6a49509b6f0f0368b90d74') 
+
+    response = requests.get(url)
+
+    article_list = []
+
+    for article in response.json()["articles"]:
+        description = article["description"]
+        url = article["url"]
+        title = article["title"]
+        date = article["publishedAt"]
+        news_html = "<div class='my-3'><a href='" + url + "' ><h4 class='font-weight-bold my-3'>"+ title+ "</h4> </a><p class='mt-1 small'>"+ date + "</p><p class='mt-2 p-2'> " + description +"</p></div>"
+        article_list.append(news_html)
+        
+
+    return article_list
